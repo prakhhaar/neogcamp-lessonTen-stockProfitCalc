da7f60bb14c87ce0a69a0fc3d19f132abb536b63
Prakhar Sharma <prakhar.shrm202@gmail.com>
2022-09-06 02:28:44 +0530

    logic done

---
+++ app.js
@@ -0,0 +1,37 @@
+var initialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitbutton = document.querySelector("#sumbit-btn");
+var outputBox = document.querySelector("#output-box");
+
+function calculateProfitAndLoss(initial, quantity, current){
+    if(initial > current){
+        var loss = (initial-current)*quantity;
+        var lossPercentage = (loss/initial)*100;
+
+        outputBox.style.outputBox = "Red";
+        showOutput("Hey, the loss is " + loss + " and the percentage is " + lossPercentage + "%");
+    } else if(current > initial ) {
+        var profit = (current - initial)*quantity;
+        var profitPercentage = (profit/initial)*100;
+
+        outputBox.style.outputBox = "Green";
+        showOutput("Hey, the profit is " + profit + " and the percentage is " + profitPercentage + "%");
+    } else {
+        showOutput("No profit, no loss.");
+    }
+}
+
+function showOutput(message){
+    outputBox.innerHTML = message;
+}
+
+function clickHandler(){
+    var ip = Number(initialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var curr = Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip, qty, curr);
+}
+
+submitbutton.addEventListener("click", clickHandler); 
